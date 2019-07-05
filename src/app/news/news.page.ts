0e687c8257656b06ee587d7f147957d933200802
abrar zahin <abrarzahin@yahoo.com>
2019-07-04 21:22:38 -0400

    10  on click to single news

--- src/app/news/news.page.ts
+++ src/app/news/news.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NewsService } from '../news.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-news',
@@ -8,19 +9,25 @@ import { NewsService } from '../news.service';
 })
 export class NewsPage implements OnInit {
   data: any;
+  page = 1;
 
-  constructor(private newsService: NewsService) { }
+  constructor(private newsService: NewsService, private router: Router) { }
 
   ngOnInit() {
     this.newsService
       .getData(
-        `everything?q=bitcoin&from=2019-06-04&sortBy=publishedAt`
+        `everything?q=bitcoin&from=2019-06-04&sortBy=publishedAt&pageSize=5&page=${
+          this.page
+        }`
       )
       .subscribe(data => {
         console.log(data);
-        this.data=data;
-  
+        this.data = data;
       });
+  }
+  onGoToNewsSinglePage(article) {
+    this.newsService.currentArticle = article;
+    this.router.navigate(['/news-single']);
   }
 
 }
